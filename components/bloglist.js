f7b2a1ef76133a93219cab39599179acac8592d5
supiritasu <supiritasu.supiritasu@gmail.com>
2024-06-18 23:57:10 +0900

    Heroの内容変更

--- components/bloglist.js
+++ components/bloglist.js
@@ -31,7 +31,7 @@ const BlogList = ({ blogs }) => {
                 <p className="leading-relaxed">{post.discription}</p>
                 <Link href={`/blog/${post.id}`}>
                   <a className="text-green-500 inline-flex items-center mt-4">
-                    Learn More
+                    Read More
                     <svg className="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round">
                       <path d="M5 12h14"></path>
                       <path d="M12 5l7 7-7 7"></path>
