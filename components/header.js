31e2225c4801db40fea159f37ca2098fb6f4cd19
supiritasu <supiritasu.supiritasu@gmail.com>
2024-06-21 00:57:17 +0900

    [id].jsの修正

--- components/header.js
+++ components/header.js
@@ -6,14 +6,14 @@ import ThemeController from "../components/ThemeController";
 
 const Header = () => {
   return (
-    <header class="text-gray-400 bg-gray-900 body-font">
-      <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+    <header className="text-gray-400 bg-gray-900 body-font">
+      <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
       <Link href="/">
-        <a class="flex title-font font-medium items-center text-white mb-4 md:mb-0">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10 text-white p-2 bg-green-500 rounded-full" viewBox="0 0 24 24">
+        <a className="flex title-font font-medium items-center text-white mb-4 md:mb-0">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-10 h-10 text-white p-2 bg-green-500 rounded-full" viewBox="0 0 24 24">
             <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
           </svg>
-          <span class="ml-3 text-xl">NextTech</span>
+          <span className="ml-3 text-xl">NextTech</span>
         </a>
       </Link>
       <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400 flex flex-wrap items-center text-base justify-center">
--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -1,16 +1,14 @@
-import PostCard from "../../components/PostCard";
 import Footer from "../../components/footer";
 import Header from "../../components/header";
 import Pagenation from "../../components/pagenation";
 import { client } from "../../libs/client";
 import styles from "../../styles/Home.module.scss";
-import { useRouter } from 'next/router';
-import { marked } from 'marked';
 
 // SSG
 export const getStaticProps = async (context) => {
   const id = context.params.id;
   const data = await client.get({ endpoint: "nexttech", contentId: id });
+  console.log(data); // デバッグ用にデータを出力
 
   return {
     props: {
@@ -21,16 +19,11 @@ export const getStaticProps = async (context) => {
 
 export const getStaticPaths = async () => {
   const data = await client.get({ endpoint: "nexttech" });
-
   const paths = data.contents.map((content) => `/blog/${content.id}`);
   return {
     paths,
     fallback: false,
   };
-};
-
-const markdownToHtml = (markdown) => {
-  return marked(markdown);
 };
 
 export default function BlogId({ blog }) {
@@ -45,51 +38,36 @@ export default function BlogId({ blog }) {
         {Array.isArray(blog.content) ? (
           blog.content.map((item, index) => (
             <div key={index} className={`${styles.post} ${styles.table}`}>
-              {item.content && (
-                <div dangerouslySetInnerHTML={{ __html: markdownToHtml(item.content) }}></div>
+              {item.main && (
+                <div 
+                  dangerouslySetInnerHTML={{ __html: item.main }}
+                  className={`${styles.post} ${styles.table}`}>
+                </div>
               )}
               {item.image && item.image.length > 0 && (
-                <div className="grid grid-cols-2 gap-4">
-                  <img
-                    src={item.image[0].url}
-                    alt="Blog Image"
-                    width={item.image[0].width}
-                    height={item.image[0].height}
-                    className="rounded-lg"
-                  />
-                  <img
-                    src={item.image[1].url}
-                    alt="Blog Image2"
-                    width={item.image[1].width}
-                    height={item.image[1].height}
-                    className="rounded-lg"
-                  />
+                <div className="flex flex-col gap-4">
+                  {item.image.map((image, idx) => (
+                    <img
+                      key={idx}
+                      src={image.url}
+                      alt={`Blog Image ${idx + 1}`}
+                      width={image.width}
+                      height={image.height}
+                      className="rounded-lg"
+                    />
+                  ))}
                 </div>
               )}
             </div>
           ))
         ) : (
-          blog.content && blog.content.content && (
+          // If blog.content is not an array, assume it's the main content
+          blog.content && (
             <div
-              dangerouslySetInnerHTML={{ __html: markdownToHtml(blog.content.content) }}
+              dangerouslySetInnerHTML={{ __html: blog.content }}
               className={`${styles.post} ${styles.table}`}
             ></div>
           )
-        )}
-
-        {blog.body && (
-          <div
-            dangerouslySetInnerHTML={{ __html: markdownToHtml(blog.body) }}
-            className={`${styles.post} ${styles.table}`}
-          ></div>
-        )}
-
-
-          {blog.content && blog.content.content && (
-          <div
-            dangerouslySetInnerHTML={{ __html: markdownToHtml(blog.content.content) }}
-            className={`${styles.post} ${styles.table}`}
-          ></div>
         )}
       </main>
 
